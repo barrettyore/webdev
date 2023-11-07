868d752cad50812db49f7549b97a54775ad99e6b
Scholar <scholar@paradigm.local>
2023-11-07 15:00:21 -0700

    vars

---
+++ data/data.js
@@ -0,0 +1,29 @@
+const var1 = "HELLO WORLD"
+const var2 = "HELLO JAVASCRIPT"
+const var3 = "lorem ipsum"
+const var4 = "fun fact lorem is not a latin word lorem is short for delorem meaning pain delorem ipsum translates to THE PAIN"
+const num1 = 13
+const num2 = 54.6
+const num3 = 78
+const bool1 = true
+const bool2 = false
+const bool3 = true
+// i really wanted to hit shift
+const array1 = ["why","not","call it a","LIST"]
+const array2 = [3,4,1]
+const array3 = [true,false,true]
+const obj1 = {
+    "name": "barrett",
+    "age": 15.5,
+    "alive": false
+}
+const obj2 = {
+    "car": "delorean",
+    "manufacturer":"dmc",
+    "model":"dmc12"
+}
+const obj3 = {
+    "name":"hunt",
+    "age":"unknown",
+    "status":"working",
+}
