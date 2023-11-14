54ed2c34644d78dfa8f2123f412812e2a65b9fe1
Scholar <scholar@paradigm.local>
2023-11-14 14:59:12 -0700

    why

--- functions/func.js
+++ functions/func.js
@@ -1,27 +1,39 @@
-function tellFortune(numOfChildren, partnerName, location, jobTitle) {
-    console.log(`you will be an ${jobTitle} in ${location} and mariied to ${partnerName} with ${numOfChildren} kids`)
+function tellFortune() {
+    numOfChildren = document.getElementById('children').value
+    partnerName = document.getElementById('partner').value
+    // location = document.getElementById('location').value
+    jobTitle = document.getElementById('job').value
+
+    document.getElementById("fortune").innerHTML = `you will be an ${jobTitle} and mariied to ${partnerName} with ${numOfChildren} kids`
 
 }
-function dog(humanYears) {
+function dog() {
+    humanYears = document.getElementById("dog").value;
     dogAge = humanYears *7
-    console.log(`dog is ${dogAge} in dog years`)
+    document.getElementById("age").innerHTML=`your dog is ${dogAge} in dog years`
 }
 
-function calculateSupply(age, amountPerDay,item) {
+function calculateSupply() {
+    age = document.getElementById("current-age").value
+    amountPerDay = document.getElementById("amount-per-day").value
     maxAge = 92
     amountPerDay = amountPerDay * 365.25
     totalAmount = (maxAge - age) * amountPerDay;
-    console.log(`you will need ${totalAmount} ${item} to last you untill your ${maxAge}`)
+    document.getElementById("supply").innerHTML = `you will need ${totalAmount} to last you untill your ${maxAge}`
 }
 
-function calCircumfrence(radius) {
-    curcumfence = 2*Math.pi *radius
+function calCircumfrence() {
+    radius = document.getElementById("radius").value
+    circumfrence = 2*Math.pi *radius
+    document.getElementById("circumfrence").innerHTML = `the circum ${circumfrence}`
     console.log(`the circum ${calCircumfrence}`)
 
 }
-function celsuisToFahrenheit(celsuis) {
-    fahrenheit = celsuis * 9 / 5 + 32
-    console.log`${celsuis}C is ${fahrenheit}F`
+function celsuisToFahrenheit() {
+    celsius = document.getElementById("c").value;
+    fahrenheit = celsius * 9 / 5 + 32
+    document.getElementById("fahrenheit").innerHTML = `${celsius}C is ${fahrenheit}F`
+    console.log(`${celsuis}C is ${fahrenheit}F`)
 
 }
 
