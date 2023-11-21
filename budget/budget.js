a9accb869510c0d18394072eae4ce75319121105
Scholar <scholar@paradigm.local>
2023-11-21 14:57:50 -0700

    masons back

---
+++ budget/budget.js
@@ -0,0 +1,24 @@
+// function calCircumfrence() {
+//     radius = document.getElementById("radius").value
+//     circumfrence = 2*Math.pi *radius
+//     document.getElementById("circumfrence").innerHTML = `the circum ${circumfrence}`
+//     console.log(`the circum ${calCircumfrence}`)
+
+// }
+
+function GO() {
+    income = document.getElementById("income").value
+    exp1 = document.getElementById("exp1").value
+    exp2 = document.getElementById("exp2").value
+    exp3 = document.getElementById("exp3").value
+    exp4 = document.getElementById("exp4").value
+    exp5 = document.getElementById("exp5").value
+    exp6 = document.getElementById("exp6").value
+    console.log(income)
+    console.log(exp1)
+    console.log(exp2)
+    console.log(exp3)
+    console.log(exp4)
+    console.log(exp5)
+    console.log(exp6)
+}
