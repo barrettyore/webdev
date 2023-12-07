30b2a3f584c6484314fad09629c2dad4d0fe026b
barrettyore <143128179+barrettyore@users.noreply.github.com>
2023-12-06 21:10:43 -0700

    Update home.js

--- final/home.js
+++ final/home.js
@@ -1,3 +1,7 @@
 const titleElement = document.getElementById('title');
 titleElement.addEventListener('click', function() {
     window.location.href = '../home.html'})
+
+document.getElementById("half-life").addEventListener("click",function() {
+    window.location.href = "half life/half life.html"
+})
