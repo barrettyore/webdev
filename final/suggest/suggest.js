ef8b83cd03c566bc02eb911fc5fa85ed5f1f05b7
barrettyore <143128179+barrettyore@users.noreply.github.com>
2023-12-08 22:30:54 -0700

    Update suggest.js

--- final/suggest/suggest.js
+++ final/suggest/suggest.js
@@ -3,11 +3,10 @@ titleElement.addEventListener('click', function() {
     window.location.href = '../home.html'})
 
 function GO() {
-    event.preventDefault()
     alert("THANK YOU FOR YOUR SUBMISSION:)")
 
     setTimeout(function() {
         location.reload(true)
-    }, 10000);
+    }, 1000);
     
 }
